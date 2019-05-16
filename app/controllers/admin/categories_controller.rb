103e2dfaafcf8b58f1d10acb5bdf259bf3e3b8f8
Sungil Saul Lee <jangsoo86@gmail.com>
2019-05-16 00:00:46 +0000

    Fix Typo

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -9,7 +9,7 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
-    @category = Category.new(catagory_params)
+    @category = Category.new(category_params)
 
     if @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
@@ -26,7 +26,7 @@ class Admin::CategoriesController < ApplicationController
 
   private
 
-  def catagory_params
+  def category_params
     params.require(:category).permit(
       :name
     )
