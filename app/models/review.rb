f68ee6134b9bd2ade5200b1b37bb8731bc7c854d
Sungil Saul Lee <jangsoo86@gmail.com>
2019-05-17 22:19:29 +0000

    add validations to the model with foreign keys

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,10 @@
 class Review < ActiveRecord::Base
+  # Add validations to a review.
+  validates :product, presence: true
+  validates :user, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
+  
+  # Review belongs to the product.
+  belongs_to :product, :user
 end
