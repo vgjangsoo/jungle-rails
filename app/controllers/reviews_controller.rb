484c1df7b3214eb0eadd2d4cb881a8df52f52c23
Sungil Saul Lee <jangsoo86@gmail.com>
2019-05-18 02:16:15 +0000

    Reverse review order, Only logged in users can review, and delete review button

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,27 @@
 class ReviewsController < ApplicationController
+  before_filter :authorize
+
   def create
-    raise "Yay, I'm here!"
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.create(review_params)
+    @review.user = current_user
+    @review.save
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+    @product = Product.find params[:product_id]
+    @review = @product.reviews.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@product)
+  end
+  
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating,
+    )
   end
 end
